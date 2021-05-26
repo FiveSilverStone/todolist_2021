85160f3031eed7f20ec2fd1cc783e2a1adaf51fd
Eunseok Oh <eunseok@cedar.kr>
2021-05-26 11:12:48 +0900

    save 버튼 추가 및 이벤트 연결

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { TextField, Typography } from '@material-ui/core';
+import { TextField, Typography, Button } from '@material-ui/core';
 import { KeyboardDatePicker, KeyboardTimePicker } from '@material-ui/pickers';
+import SaveIcon from '@material-ui/icons/Save';
 
 import './App.css';
 class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
+      todoList: [],
       title: "",
       content: "",
       startDate: null,
@@ -14,20 +16,41 @@ class App extends React.Component{
     }
   }
 
+  saveTodoList(){
+    const { todoList, title, content, startDate, startTime } = this.state;
+    todoList.push({title, content, startDate, startTime});
+    this.setState({ 
+      todoList,
+      title: "",
+      content: "",
+      startDate: null,
+      startTime: null,
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="header">TODO LIST</div>
         <div className="input_area">
-          <TextField label="제목" size="normal" margin="normal" fullWidth required />
-          <TextField label="상세내용" size="normal" margin="normal" fullWidth multiline />
+          <TextField 
+            label="제목" size="normal" margin="normal" fullWidth required 
+            value={this.state.title}
+            onChange={(e)=> this.setState({title:e.target.value})} 
+          />
+          <TextField
+           label="상세내용" size="normal" margin="normal" fullWidth multiline
+           value={this.state.content}
+           onChange={(e)=> this.setState({content:e.target.value})}   
+          />
           <KeyboardDatePicker
             disableToolbar
             variant="inline"
             format="yyyy/MM/DD"
             margin="normal"
             label="시작 예정일"
-            onChange={(value)=>console.log(value)} 
+            value={this.state.startDate}
+            onChange={(value)=> this.setState({startDate:value})} 
             style = {{width: '50%'}}     
             KeyboardButtonProps={{
               'aria-label': 'change date',
@@ -37,12 +60,21 @@ class App extends React.Component{
             margin="normal"
             label="시작시간"
             variant="inline"
-            onChange={(value)=>console.log(value)}   
+            value={this.state.startTime}
+            onChange={(value)=> this.setState({startTime:value})} 
             style = {{width: '50%'}}   
             KeyboardButtonProps={{
               'aria-label': 'change time',
             }}
           />
+          <Button
+            variant="outlined"
+            startIcon={<SaveIcon />}
+            style={{float:'right'}}
+            onClick={()=>this.saveTodoList()}
+          >
+            Save
+          </Button>
         </div>
         <div className="list_area">리스트 영역</div>
         <Typography variant="body2" color="textSecondary" align="center">
